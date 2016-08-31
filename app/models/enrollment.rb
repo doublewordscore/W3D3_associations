1cfb3333b47d844a65c736208c8b860c990f3cef
App Academy Student <appacademy@Apps-Mac-mini-95.local>
2016-08-31 12:01:33 -0400

    complete associations exercise with user, course, enrollment

---
+++ app/models/enrollment.rb
@@ -0,0 +1,14 @@
+class Enrollment < ActiveRecord::Base
+
+  belongs_to :user,
+    class_name: "User",
+    foreign_key: :student_id,
+    primary_key: :id
+
+
+  belongs_to :courses,
+    class_name: "Course",
+    foreign_key: :course_id,
+    primary_key: :id
+
+end
